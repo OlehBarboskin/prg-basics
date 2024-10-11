c777164b4745b525a229f17843ae6e72f0339e7e
OlehBarboskin <iseeassyt@gmail.com>
2024-10-11 09:53:30 +0200

    2-2 to 2-6 tasks section 3

---
+++ 02-TypesAndVariables/2-3.py
@@ -0,0 +1,14 @@
+x = 7
+y = 34
+z = 0
+o = 0
+print("Before swapping: x=", x, "y=", y)
+
+# swap the values
+o = x
+z = y
+x = z
+y = o
+...
+
+print("After swapping: x=", z, "y=", o)
---
+++ 02-TypesAndVariables/2-6.py
@@ -0,0 +1,6 @@
+import math
+h = input ('h=')
+d = 3.57 * math.sqrt(h)
+print (d)
+x = h + 20
+print (x)
