ffeafa1ab871885a407584d27b12442122a8ecb2
OlehBarboskin <iseeassyt@gmail.com>
2024-10-25 10:06:31 +0200

    5-4 full

---
+++ 04-Functions/5-1 (func).py
@@ -0,0 +1,23 @@
+###
+# Program for testing built-in functions
+#
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+input_value = input("Write some letters:")
+print (input_value)
+int_value = int ("20303")
+print (int_value)
+bin_string = bin (304)
+print (bin_string)
+hex_decimal = hex(304)
+uni_int = ord("€")
+print (uni_int)
+abs_val = abs(-17)
+print (abs_val)
---
+++ 04-Functions/5-4 (func).py
@@ -0,0 +1,9 @@
+def sum_digits(number):
+   a = int (input abs(("")))
+    thing = str(a)
+   for thing in 
+    return ...
+
+any_number = int(input('Enter integer number: '))
+result = sum_digits(...)
+print('The sum of the digits in the number ... is ...')
